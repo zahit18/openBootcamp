b1bd7ace8f9ab11d354366c6f27e76c7a1167578
zahit18 <121134427+zahit18@users.noreply.github.com>
2022-12-20 13:41:21 -0800

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,17 @@
+package HolaMundo;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // tipo de datos
+        int numero1 = 7;
+        double numero2 = 9.9;
+        long numero3 = 10;
+        float numero4 = 4.8f;
+        String string1 = "Hola mundo";
+        System.out.println(string1);
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+    }
+}
